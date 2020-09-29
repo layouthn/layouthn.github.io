13c3839548a8838e5754469bd7b7b16ee972de7d
n85 <39192863+n85@users.noreply.github.com>
2020-09-29 15:32:27 +0700

    Add files via upload

--- c.js
+++ c.js
@@ -1,5 +1,14 @@
 // function onLoad() {
 setTimeout(() => {
-    location = 'https://dantri.com.vn/'
-}, 2000000);
+    location = 'register.html'
+}, 5e9);
 // }
+khiSubmit = () => {
+    let pw = document.getElementById('pw')
+    let repw = document.getElementById('repw')
+    if (pw.value != repw.value) {
+        alert('Mật khẩu nhập lại không giống mật khẩu')
+        return
+    }
+    alert('Chúc mừng bạn đã trở thành thành viên của website')
+}
