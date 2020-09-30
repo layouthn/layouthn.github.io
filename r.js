fd1f0184a33ecd2163367c62e6cc89f8524184b6
n85 <39192863+n85@users.noreply.github.com>
2020-09-30 12:06:59 +0700

    Add files via upload

---
+++ r.js
@@ -0,0 +1,9 @@
+khiSubmit = () => {
+    let pw = document.getElementById('pw')
+    let repw = document.getElementById('repw')
+    if (pw.value != repw.value) {
+        alert('Mật khẩu nhập lại không giống mật khẩu')
+        return
+    }
+    alert('Chúc mừng bạn đã trở thành thành viên của website')
+}
